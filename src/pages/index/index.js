b2b8b53e8258082e71283866437e85e01fde5e5e
mumu111111 <2646709035@qq.com>
2019-11-30 10:38:05 +0800

    '使用mint-ui组件库的loadmore组件'

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -4,11 +4,15 @@ import './index.css'
 import Vue from 'vue'
 import axios from 'axios'
 import url from 'js/api.js'
+import { Loadmore } from 'mint-ui';
+
+Vue.component(Loadmore.name, Loadmore);
 
 new Vue({
     el: '.container',
     data: {
-        lists: null
+        lists: null,
+        allLoaded: false
     },
     created() {
         this.getLists()
@@ -19,6 +23,15 @@ new Vue({
                 this.lists = res.data.lists
             })
         }
-    }
+    },
+    loadBottom() {
+        console.log('load')
+        // 加载更多数据
+        // this.allLoaded = true;// 若数据已全部获取完毕
+        // this.$refs.loadmore.onBottomLoaded();
+    },
+    handleBottomChange(status) {
+        this.topStatus = status;
+    },
 
 })
