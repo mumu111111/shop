54234861f0d0c2d2e52d144c27fef7aab661d79a
mumu111111 <2646709035@qq.com>
2019-12-04 23:07:29 +0800

    '商品选择/店铺选择/全选checked'

--- src/pages/cart/cart.js
+++ src/pages/cart/cart.js
@@ -32,5 +32,58 @@ new Vue({
 
 
         })
+    },
+    computed: {
+        allSelect: {
+            get() {
+                if (this.cartlist && this.cartlist.length) {//以来的data必须存在
+                    return this.cartlist.every(item => {
+                        return item.checked
+                    })
+                }
+                return false
+            },
+            set(newVal) {
+                // if (this.cartlist && this.cartlist.length) {//以来的data必须存在
+
+                //     this.cartlist.forEach(shop => { //全选 选中后 商品店铺全被选中  未全选 所有商品未选中
+                //         shop.checked = newVal
+                //         shop.goodsList.forEach(item => {
+                //             item.checked = newVal
+                //         })
+                //     })
+
+                // }
+
+                this.cartlist.forEach(item => {
+                    console.dir('item' + item)
+                    item.checked = newVal
+                    item.goodsList.forEach(good => {
+                        good.checked = newVal
+                    })
+                })
+            }
+        }
+    },
+    methods: {
+        selectGood(good, shop) {
+            good.checked = !good.checked //状态切换
+            shop.checked = shop.goodsList.every(item => {//判断是不是所有商品都是选中状态
+                return item.checked
+            })
+
+        },
+        selectShop(shop) {
+            shop.checked = !shop.checked
+            shop.goodsList.forEach(item => { //选中店铺 那么所哟商品也全部选中
+                item.checked = shop.checked
+            })
+        },
+        selectAll() {
+            this.allSelect = !this.allSelect
+
+
+        }
+
     }
 })
