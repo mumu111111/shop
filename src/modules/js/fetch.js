e5b9a2604c57eaf2226eebb88058bb3460399701
mumu111111 <2646709035@qq.com>
2019-12-10 01:08:36 +0800

    '请求封装'

---
+++ src/modules/js/fetch.js
@@ -0,0 +1,72 @@
+import axios from 'axios'
+function fetch(method='GET',url,data={}){
+    return new Promise((resolve,reject)=> {
+        let options = {method, url}
+        if(method.toLowerCase() === 'get'){
+            options.params = data
+        }else{
+            options.data = data
+        }
+        axios.post(url, data).then(res=> {
+            let status = res.data.status
+            if(status === 200){
+                resolve(res)
+            }
+            if(status > 300){
+                location.href = "login.html"
+                resolve(res)
+            }
+            reject(res)
+        }).catch(error=> {
+            reject(error)
+        })
+    })
+}
+
+
+export default fetch
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import url from 'js/api.js'
+
+// function fetch({method,url}){
+//     return new Promise((resolve,reject)=>{
+//         if(method.lowerCase ==='Post'){
+
+//         }
+//         axios.post(url, {data}).then(res=>{
+//             if(res.status == 200){
+//                 resolve(res.data)
+//             }else{
+//                 reject(res)
+//             }
+//         }).catch(error){
+//             console.log(error)
+//         }
+//     })
+// }
